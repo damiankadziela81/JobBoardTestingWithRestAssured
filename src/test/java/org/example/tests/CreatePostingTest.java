2568c9a376c9ceb3b55bc42c5c59a8ae2af56f02
damiankadziela81 <damiankadziela02@gmail.com>
2023-07-20 14:13:23 +0200

    Inital commit

---
+++ src/test/java/org/example/tests/CreatePostingTest.java
@@ -0,0 +1,43 @@
+package org.example.tests;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.assertj.core.api.Assertions;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+class CreatePostingTest {
+
+    private static final String BASE_URL = "http://localhost:8080/postings";
+
+    @Test
+    void createPostingTest() {
+
+        JSONObject company = new JSONObject();
+        company.put("name", "Valve");
+        company.put("contact", "gaben@valve.com");
+        company.put("website", "https://valve.com");
+
+        JSONObject jobPosting = new JSONObject();
+        jobPosting.put("description", "job from Java");
+        jobPosting.put("salary", 8000);
+        jobPosting.put("expiresAt", "2024-08-01");
+        jobPosting.put("company", company);
+
+        final Response response = given()
+                .contentType(ContentType.JSON)
+                .body(jobPosting.toString())
+                .when()
+                .post(BASE_URL)
+                .then()
+                .log().ifError()
+                .extract()
+                .response();
+
+        Assertions.assertThat(response.statusCode()).isEqualTo(201);
+        Assertions.assertThat(response.jsonPath().getString("company.name")).isEqualTo("Valve");
+
+    }
+}
